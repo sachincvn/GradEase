07104bf0c23caa730d43f806e92f0d0b3ff8192d
sachin-chavan <sachincvn962@gmail.com>
2024-03-29 10:56:33 +0530

    removed all controllers

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const AuthRoutes = require("./routes/AuthRoute");
-const StudentRoutes = require("./routes/StudentRoute");
 require("dotenv").config();
 require("./config/dbConfig");
 
@@ -11,15 +9,9 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 8080;
 
-const ApiBasePath = "/api/v1";
-
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to GradEase" });
 });
-
-app.use(ApiBasePath, AuthRoutes);
-
-app.use(ApiBasePath, StudentRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
