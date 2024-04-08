3e43d93bcc57c1d4e10b611c145a9e69c09b538f
sachin-chavan <sachincvn962@gmail.com>
2024-04-08 19:45:25 +0530

    added rquired model and entity for replies

---
+++ lib/features/feeds/data/models/feed_post_reply_model.dart
@@ -0,0 +1,28 @@
+import 'package:grad_ease/features/feeds/data/models/author_model.dart';
+import 'package:grad_ease/features/feeds/domain/enitity/feed_post_reply_entity.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'feed_post_reply_model.g.dart';
+
+@JsonSerializable()
+class FeedPostReplyModel {
+  @JsonKey(name: '_id')
+  final String id;
+  final String? content;
+  final Author author;
+
+  FeedPostReplyModel({
+    required this.id,
+    required this.content,
+    required this.author,
+  });
+
+  FeedPostReplyEntity toEntity() {
+    return FeedPostReplyEntity(content: content, author: author);
+  }
+
+  factory FeedPostReplyModel.fromJson(Map<String, dynamic> json) =>
+      _$FeedPostReplyModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$FeedPostReplyModelToJson(this);
+}
---
+++ lib/features/feeds/domain/enitity/feed_post_reply_entity.dart
@@ -0,0 +1,8 @@
+import 'package:grad_ease/features/feeds/data/models/author_model.dart';
+
+class FeedPostReplyEntity {
+  final String? content;
+  final Author author;
+
+  FeedPostReplyEntity({required this.content, required this.author});
+}
