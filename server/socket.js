c65b3dcd0122c86059acdc5a60f3eeb62d67bd75
sachin-chavan <sachincvn962@gmail.com>
2024-04-25 00:16:11 +0530

    removed logs

--- server/socket.js
+++ server/socket.js
@@ -6,21 +6,12 @@ const initializeSocket = (server) => {
   io = new Server(server);
 
   io.on("connection", (socket) => {
-    console.log("A user connected");
-
     socket.on("joinCommunity", (communityId) => {
       socket.join(communityId);
     });
 
     socket.on("chatMessage", (data) => {
       io.to(data.communityId).emit("newMessage", { message: data.message });
-      console.log(
-        `Message received in room ${data.communityId}: ${data.message}`
-      );
-    });
-
-    socket.on("disconnect", () => {
-      console.log("A user disconnected");
     });
   });
 };
